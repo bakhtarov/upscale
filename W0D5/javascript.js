d4ce61b4013dc3a4177bc6d0ff75fe30653c5cd1
George <bakhtarov@gmail.com>
2017-04-07 21:58:16 +0300

    recognizing a winner, stop a game after it

--- W0D5/javascript.js
+++ W0D5/javascript.js
@@ -9,13 +9,25 @@ window.onload = function () {
 		restart = id("restart"),
 		friend = id('friend'),
 		cross = id('cross'),
-		nought = id('nought');
+		nought = id('nought'),
+		counter = 1;
 
 
 	newGame.addEventListener('click', fillIn);
 	restart.addEventListener('click', cleanUp);
 
+	function combo (x, y, z) {
+		return 	tdArr[x].innerHTML.length && 
+				tdArr[x].innerHTML == tdArr[y].innerHTML &&
+				tdArr[y].innerHTML == tdArr[z].innerHTML;
+	}
 
+	function cleanUp () {
+		for (var i = 0; i < tdArr.length; i++) {
+				tdArr[i].innerHTML = '';
+				counter = 1;
+		}
+	}
 
 
 	function fillIn () {
@@ -23,35 +35,51 @@ window.onload = function () {
 		cleanUp();
 
 		if (friend.checked) {
-			var counter = 1;
-			
+			counter = 1;
 			for (var i = 0; i < tdArr.length; i++) {
-					tdArr[i].addEventListener('click', function () {
-		 				if (counter % 2) {
-		 					counter++;
-							this.innerHTML = 'X';
+
+				tdArr[i].addEventListener('click', function (e) {
+					if (combo(0, 1, 2) || combo(3, 4, 5) || combo(6, 7, 8) || combo(0, 3, 6) ||
+						combo(1, 4, 7) || combo(2, 5, 8) || combo(0, 4, 8) || combo(2, 6, 4)) {
+						 alert("we've got a winner".toUpperCase());
+						 e.preventDefault(); 
+					} else {
+						if (this.innerHTML.length == 0) {
+			 				if (counter % 2) {
+			 					counter++;
+								this.innerHTML = 'X';
+							} else {
+			 					counter++;
+								this.innerHTML = 'O';
+							}
 						} else {
-		 					counter++;
-							this.innerHTML = 'O';
+							i--;
 						}
-					});
+					}
+				});
+
+				tdArr[i].addEventListener('mouseover', function  () {
+					this.style.cursor = 'pointer';
+				});
+
 			}
+
 			
 		} else if (cross.checked) {
 			for (var i = 0; i < tdArr.length; i++) {
-				tdArr[i].addEventListener('click', function () {
+				tdArr[i].addEventListener('click', function (e) {
 					this.innerHTML = 'X';
 				})
 			}
 		} else {
 			for (var i = 0; i < tdArr.length; i++) {
-				tdArr[i].addEventListener('click', function () {
+				tdArr[i].addEventListener('click', function (e) {
 					this.innerHTML = 'O';
 				})
-				// tdArr[i].addEventListener('mouseover', function  () {
-				// 	this.style.cursor = 'pointer';
-				// 	this.style.color = 'brown';
-				// })
+				tdArr[i].addEventListener('mouseover', function  () {
+					this.style.cursor = 'pointer';
+					// this.style.color = 'brown';
+				})
 
 				// tdArr[i].addEventListener('mouseout', function  () {
 				// 	this.style.color = '';
@@ -59,13 +87,4 @@ window.onload = function () {
 			}
 		}		
 	}
-
-
-	function cleanUp () {
-		for (var i = 0; i < tdArr.length; i++) {
-				tdArr[i].innerHTML = '';
-
-		}
-	}
-
 }
